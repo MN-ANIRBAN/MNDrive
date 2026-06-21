cfcefcd1f0ac62884036a3c32d8e5e2f7653d9f8
ANIRBAN ADHIKARY <54547722+MN-ANIRBAN@users.noreply.github.com>
2026-06-21 23:48:59 +0530

    Refactor GET function in auth callback route

--- MNCloudDrive/src/app/auth/callback/route.ts
+++ MNCloudDrive/src/app/auth/callback/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  // যদি কোন 'next' প্যারামিটার থাকে (লগইনের পর নির্দিষ্ট কোন পেজে পাঠানোর জন্য)
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
@@ -19,6 +18,5 @@ export async function GET(request: Request) {
   }
 
 
-  // যদি কোন সমস্যা হয়, তবে ইউজারকে এরর মেসেজ সহ আবার লগইন পেজে ফেরত পাঠাবে
   return NextResponse.redirect(`${origin}/login?message=Could not invalidate auth token.&type=error`)
 }
